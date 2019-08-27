06fe17d45d7ac60fa78d836e9732be8a002f880b
Joe <joeraz@gmail.com>
2019-08-27 22:15:08 +0100

    Update ex41.py

--- ex41.py
+++ ex41.py
@@ -5,4 +5,12 @@ import sys
 WORLD_UR = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
-PHRASES
+PHRASES = {
+        "class %%%(%%%):":
+            "Make a class named %%% that is-a %%%.",
+            }
+# do they want to drill phrases first
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+    PHRASE_FIRST = True
+else:
+    PHRASE_FIRST = False
