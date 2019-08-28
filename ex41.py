465eeb5d41b6c279d36b94bc8a16d88327672cfc
Joe <joeraz@gmail.com>
2019-08-28 11:48:02 +0100

    Update ex41.py

--- ex41.py
+++ ex41.py
@@ -8,9 +8,23 @@ WORDS = []
 PHRASES = {
         "class %%%(%%%):":
             "Make a class named %%% that is-a %%%.",
+        "class %%%(object):\n\tdef __init__(self, ***)" :
+            "Class %%% has-a __init__ that takes self and *** params.",
             }
 # do they want to drill phrases first
 if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
 else:
     PHRASE_FIRST = False
+
+# load up the words from the website
+for word in urlopen(WORD_URL).readlines()
+    WORDS.append(str(word.strip(), enconding="utf-8"))
+
+
+def convert(snippet, phrase):
+    class_name = [w.capitalize() for w in
+                    random.sample(WORDS, snippet.count("%%%"))]
+other_names = random.sample(WORDS, snippet.count("%%%"))
+results = []
+param_names = []
